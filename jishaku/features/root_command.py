04d70b6730ef11b9d65165dbe6ea7ab57c5648c1
HARSH <96734555+Jod-Harsh@users.noreply.github.com>
2022-12-07 21:41:41 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -107,7 +107,7 @@ class RootCommand(Feature):
                 )
                 summary.append("")  # blank line
 
-        cache_summary = f"{len(self.bot.guilds)} guild(s) and {len(self.bot.users)} user(s)"
+        cache_summary = f"Total {len(self.bot.guilds)} guilds in bot"
 
         # Show shard settings to summary
         if isinstance(self.bot, discord.AutoShardedClient):
